324ad8c5cc5563e46bf3b092a91e98bc834d0aa8
Ishika <Ishikakamra05@gmail.com>
2026-08-21 11:58:52 +0530

    Explicitly bind Java HttpServer to 0.0.0.0 for Docker networking

--- backend/src/Server.java
+++ backend/src/Server.java
@@ -13,7 +13,7 @@ public class Server {
 
     public static void main(String[] args) throws Exception {
 
-        HttpServer server = HttpServer.create(new InetSocketAddress(7070),0);
+        HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", 7070), 0);
 
         server.createContext("/test", exchange -> {
 
